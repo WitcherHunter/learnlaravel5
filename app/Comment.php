e86340e00b76399cc642893b7e3adf531baa518e
z34771914 <gmzhjz@gmail.com>
2017-04-16 23:28:18 +0800

    完成评论模块

--- app/Comment.php
+++ app/Comment.php
@@ -6,4 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model {
     protected $fillable = ['nickname', 'email', 'website', 'content', 'article_id'];
+
+    public function post(){
+        return $this->hasOne('App\Article','id','article_id');
+    }
 }
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,15 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Dashboard</div>
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
 
-				<div class="panel-body">
-					<a href="{{ url('admin/article') }}" class="btn btn-lg btn-success col-xs-12">管理文章</a>
-				</div>
-			</div>
-		</div>
-	</div>
+                <div class="panel-body">
+                    <div style="padding: 20px;">
+                        <a href="{{ url('admin/article') }}" class="btn btn-lg btn-success col-xs-12">管理文章</a>
+                    </div>
+                    <div style="padding: 20px;">
+                        <a href="{{ url('admin/comment') }}" class="btn btn-lg btn-success col-xs-12">管理评论</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
